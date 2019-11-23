ae989f4e31ec8a1b783b2cc6fa780aa408651a80
Lusa Balbinot <181324@lci-6-12.UPF>
2019-11-23 16:13:08 -0300

    first commit

---
+++ src/app/features/footr/footr.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footr',
+  templateUrl: './footr.component.html',
+  styleUrls: ['./footr.component.scss']
+})
+export class FootrComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
